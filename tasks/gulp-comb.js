41898e297334fcce818835fda8af9de78cc578f7
wphogan <hogan.wp@gmail.com>
2016-10-15 13:23:44 -0700

    set comb to ignore vendor scss

--- tasks/gulp-comb.js
+++ tasks/gulp-comb.js
@@ -3,7 +3,7 @@
 //
 module.exports = function (gulp, plugins, theme_dir, sass_dir, css_dir) {
     return function () {
-        return gulp.src(sass_dir + "*.scss")
+        return gulp.src([sass_dir + "**/*.scss", '!' + sass_dir + "/external/**/*.scss"])
           .pipe(plugins.csscomb('./config/csscomb_config.json'))
           .pipe(gulp.dest(sass_dir));
     };
